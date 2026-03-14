8321477ca0f34a478385a977c238255abcb9fb42
OMSAH <huncyoms@gmail.com>
2026-03-14 19:58:12 +0530

    added

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
-import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
+
 import authRoutes from "./routes/authRoutes.js";
 import organizationRoutes from "./routes/organization.routes.js";
 import patientRoutes from "./routes/patient.routes.js";
@@ -12,19 +15,24 @@ import userRoutes from "./routes/user.routes.js";
 import campRoutes from "./routes/campRoutes.js"
 
 
-
-dotenv.config();
 const app = express();
 
-app.use(
-    cors({
-        origin: [process.env.CORS_ORIGIN],
-        credentials: true,
-    })
-);
+connectDB();
+
 app.use(express.json());
 
-connectDB();
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN || "*",
+    credentials: true,
+  })
+);
+
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
 
 app.use("/api/auth", authRoutes);
@@ -38,6 +46,7 @@ app.use("/api/camps", campRoutes);
 
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () =>
     console.log(`Server running on port ${PORT}`)
 );
